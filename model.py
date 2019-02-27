bcac44b4210dc735b12b5daccb241cc8e57f7e2b
Brandon <brandon.morgan01@gmail.com>
2019-02-26 22:10:53 -0700

    5.

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from sklearn.cluster import KMeans
